94b84e2d5adab5fc80ecec579a36f25ee6a2c88c
zKnight79 <guillaume.jibaut@live.com>
2022-11-23 14:24:07 +0100

    Configure Mongo Cluster

--- OrleansPOC.ClientWorker/Program.cs
+++ OrleansPOC.ClientWorker/Program.cs
@@ -1,13 +1,26 @@
 using OrleansPOC.ClientWorker;
 
 await Host.CreateDefaultBuilder(args)
-    .UseOrleansClient(clientBuilder =>
+    .UseOrleansClient((ctx, clientBuilder) =>
     {
-        clientBuilder.UseLocalhostClustering();
+        string? mongoCS = ctx.Configuration["MongoProvider:ConnectionString"];
+        string? mongoDN = ctx.Configuration["MongoProvider:DatabaseName"];
+        if (string.IsNullOrWhiteSpace(mongoCS) || string.IsNullOrWhiteSpace(mongoDN))
+        {
+            clientBuilder.UseLocalhostClustering();
+        }
+        else
+        {
+            clientBuilder.UseMongoDBClient(mongoCS);
+            clientBuilder.UseMongoDBClustering(options =>
+            {
+                options.DatabaseName = mongoDN;
+            });
+        }
     })
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
-        services.Configure<ConsoleLifetimeOptions>(options => options.SuppressStatusMessages= true);
+        services.Configure<ConsoleLifetimeOptions>(options => options.SuppressStatusMessages = true);
     })
     .RunConsoleAsync();
--- OrleansPOC.ServerWorker/Program.cs
+++ OrleansPOC.ServerWorker/Program.cs
@@ -1,17 +1,28 @@
 await Host.CreateDefaultBuilder(args)
     .UseOrleans((ctx, siloBuilder) =>
     {
-        siloBuilder.UseLocalhostClustering();
-
         string? mongoCS = ctx.Configuration["MongoProvider:ConnectionString"];
         string? mongoDN = ctx.Configuration["MongoProvider:DatabaseName"];
+        bool mongoClustering = ctx.Configuration.GetValue<bool>("MongoProvider:UseClustering");
         if (string.IsNullOrWhiteSpace(mongoCS) || string.IsNullOrWhiteSpace(mongoDN))
         {
+            siloBuilder.UseLocalhostClustering();
             siloBuilder.AddMemoryGrainStorageAsDefault();
         }
         else
         {
             siloBuilder.UseMongoDBClient(mongoCS);
+            if (mongoClustering)
+            {
+                siloBuilder.UseMongoDBClustering(options =>
+                {
+                    options.DatabaseName = mongoDN;
+                });
+            }
+            else
+            {
+                siloBuilder.UseLocalhostClustering();
+            }
             siloBuilder.AddMongoDBGrainStorageAsDefault(options =>
             {
                 options.DatabaseName= mongoDN;
