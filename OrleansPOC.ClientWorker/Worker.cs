c33b303a9996b7c8221c758ed7deee074e60b2b6
zKnight79 <guillaume.jibaut@live.com>
2022-11-25 14:06:57 +0100

    Use Regexes to parse commands

--- OrleansPOC.ClientWorker/Worker.cs
+++ OrleansPOC.ClientWorker/Worker.cs
@@ -1,13 +1,16 @@
 using OrleansPOC.GrainInterfaces;
+using System.Text.RegularExpressions;
 
 namespace OrleansPOC.ClientWorker
 {
-    public class Worker : BackgroundService
+    public partial class Worker : BackgroundService
     {
         private const string QUIT_CMD = "/quit";
         private const string HELP_CMD = "/help";
         private const string HELLO_CMD = "/hello";
         private const string PRODUCT_CMD = "/prod";
+        private const string NAME_ARG = "name";
+        private const string CODE_ARG = "code";
 
         private readonly IClusterClient _clusterClient;
         private readonly IHostApplicationLifetime _applicationLifetime;
@@ -16,6 +19,63 @@ namespace OrleansPOC.ClientWorker
         {
             _clusterClient = clusterClient;
             _applicationLifetime = applicationLifetime;
+        }
+
+        [GeneratedRegex($"{QUIT_CMD}")]
+        private static partial Regex QuitCommandRegex();
+
+        [GeneratedRegex($"{HELP_CMD}")]
+        private static partial Regex HelpCommandRegex();
+
+        [GeneratedRegex(@$"{HELLO_CMD} (?<{NAME_ARG}>.+)")]
+        private static partial Regex HelloCommandRegex();
+
+        [GeneratedRegex(@$"{PRODUCT_CMD} (?<{CODE_ARG}>\w+) (?<{NAME_ARG}>.+)")]
+        private static partial Regex ProductCommandRegex();
+
+        private static bool ParseQuitCommand(string cmd)
+        {
+            Match match = QuitCommandRegex().Match(cmd);
+            return match.Success;
+        }
+        private static bool ParseHelpCommand(string cmd)
+        {
+            Match match = HelpCommandRegex().Match(cmd);
+            return match.Success;
+        }
+        private static bool ParseHelloCommand(string cmd, out string name)
+        {
+            bool result = false;
+            
+            if (HelloCommandRegex().Match(cmd) is { Success: true } matchHello)
+            {
+                name = matchHello.Groups[NAME_ARG].Value;
+                result = true;
+            }
+            else
+            {
+                name = "";
+            }
+
+            return result;
+        }
+        private static bool ParseProductCommand(string cmd, out string code, out string name)
+        {
+            bool result = false;
+
+            if (ProductCommandRegex().Match(cmd) is { Success: true } matchProd)
+            {
+                code = matchProd.Groups[CODE_ARG].Value;
+                name = matchProd.Groups[NAME_ARG].Value;
+                result = true;
+            }
+            else
+            {
+                code = "";
+                name = "";
+            }
+
+            return result;
         }
 
         private static void ShowHelp()
@@ -37,39 +97,28 @@ namespace OrleansPOC.ClientWorker
             {
                 Console.WriteLine("Enter your command :");
                 string? cmd = Console.ReadLine();
-                if (cmd is null or QUIT_CMD)
+                if (cmd is null || ParseQuitCommand(cmd))
                 {
                     _applicationLifetime.StopApplication();
                     return;
                 }
-                else if (cmd is HELP_CMD)
+                else if (ParseHelpCommand(cmd))
                 {
                     ShowHelp();
                 }
-                else if (cmd.StartsWith(HELLO_CMD))
+                else if (ParseHelloCommand(cmd, out string helloName))
                 {
                     IHello helloGrain = _clusterClient.GetGrain<IHello>(Guid.NewGuid());
-                    string name = cmd.Replace(HELLO_CMD, "").Trim();
-                    string hello = await helloGrain.SayHello(name);
+                    string hello = await helloGrain.SayHello(helloName);
                     Console.WriteLine($"[{helloGrain.GetPrimaryKey()}] said `{hello}`");
                 }
-                else if (cmd.StartsWith(PRODUCT_CMD))
+                else if (ParseProductCommand(cmd, out string prodCode, out string prodName))
                 {
-                    string[] args = cmd.Split(' ');
-                    if (args.Length == 3)
-                    {
-                        string code = args[1];
-                        string name = args[2];
-                        Console.WriteLine($"Try getting product `{code}` to set name to `{name}`");
-                        IProduct productGrain = _clusterClient.GetGrain<IProduct>(code);
-                        string oldName = await productGrain.GetName();
-                        Console.WriteLine($"`{code}` product name was `{oldName}`");
-                        await productGrain.SetName(name);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Product command needs exactly 2 arguments");
-                    }
+                    Console.WriteLine($"Try getting product `{prodCode}` to set name to `{prodName}`");
+                    IProduct productGrain = _clusterClient.GetGrain<IProduct>(prodCode);
+                    string oldName = await productGrain.GetName();
+                    Console.WriteLine($"`{prodCode}` product name was `{oldName}`");
+                    await productGrain.SetName(prodName);
                 }
                 else
                 {
